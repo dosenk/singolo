e76a9ed44e02761ca1db60e1f7567700c5ff80c3
Dmitry Osipenko <eympel@gmail.com>
2020-03-15 15:48:23 +0300

    fix: fixed styles and added selection of menu block

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 window.onload = () => {
 
-
+    moveToActiveSection();
 
     
+}
+
+const moveToActiveSection = () => {
+    document.querySelector('.navigation').addEventListener('click', (e) => {
+        // console.log(e.target.classList);
+        if (e.target.classList.contains('nav')) {
+            let clickedTag = e.target;
+            removeSelectedTags();
+        }
+    });
+    // console.log(element);
+    // element.forEach(el => {
+    //     el.addEventListener('click');
+    // });
+}
+
+const removeSelectedTags = () => {
+    let tags = document.querySelectorAll('.navigation .nav');
+    console.log(tags);
+    tags.forEach(tag => {
+        tag.classList.remove('active');
+    });
 }
