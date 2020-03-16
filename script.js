ff77809abedcb3a93dd06df9083fdf9762babf66
Dmitriy <eympel@gmail.com>
2020-03-16 19:03:28 +0300

    feat: added slide switching in the main unit and fixed turning on/off the phone display

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 window.onload = () => {
-
+    // header
     moveToActiveSection();
-
+    // main
     turnScreen();
     slideMainDisplay();
- 
+    
     
 }
 // ############################# header ############################################
@@ -34,17 +34,41 @@ const selectCleckedTag = (clicedTag) => {
 const turnScreen = () => {
     let slider = document.querySelectorAll('.slider__images_display');
     slider.forEach(slid => {
-        slid.addEventListener('click', () => event.target.style.opacity = event.target.style.opacity == 1 ? 0 : 1);
+        slid.firstElementChild.addEventListener('click', 
+        () => event.toElement.nextElementSibling.style.opacity = event.toElement.nextElementSibling.style.opacity == 1 ? 0 : 1);
     })
 }
 
 const slideMainDisplay = () => {
-    previousSlide();
-nextSlide(); 
+    const slides = Array.from(document.querySelectorAll('.slider_container__phone'));
+    const left = document.querySelector('.slider__next');
+    const right = document.querySelector('.slider__prev');
+    const sliderContainer = document.querySelector('.slider_container');
+    let step = sliderContainer.offsetWidth;
+    let currentSlide = 0;
+    let position = 0;
+    left.addEventListener('click', () => {
+        currentSlide = slideTransform('left', currentSlide, slides);
+    })
+    right.addEventListener('click', () => {
+    currentSlide = slideTransform('right', currentSlide, slides);
+    })
+    const slideTransform = (direction, currentSlide, slides) => {    
+        if (direction === 'left') {
+          position--;
+          if (currentSlide - 1 < 0) currentSlide = slides.length - 1;
+          else currentSlide = currentSlide - 1;
+          slides[currentSlide].style.transform = `translateX(${(-position)* step - currentSlide * step}px)`
+          sliderContainer.style.transform = `translateX(${position * step}px)`
+    
+        }
+        if (direction === 'right') {
+          position++;
+          if (currentSlide + 1 > slides.length - 1) currentSlide = 0;
+          else currentSlide = currentSlide + 1;
+          slides[currentSlide].style.transform = `translateX(${(-position)* step - currentSlide * step}px)`
+          sliderContainer.style.transform = `translateX(${position * step}px)`;
+        }
+        return currentSlide;
+      }
 }
-
-
-const previousSlide = () => {
-
-}
-
