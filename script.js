e750b5d79ac8cdfa68c563e46a2b90c41546f6b1
dosenk <eympel@gmail.com>
2020-03-18 17:58:31 +0300

    feat: add function shuffle images in section portfolio

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 window.onload = () => {
-
+    // let step = 0;
     moveToActiveSection();
     // main
     turnScreen();
     slideMainDisplay();
     // Portfolio
-
+    shufflePortfolio();
 
 
 }
@@ -16,16 +16,16 @@ const moveToActiveSection = () => {
     document.querySelector('.navigation').addEventListener('click', (e) => {
         if (e.target.classList.contains('nav')) {
             let clickedTag = e.target;
-            const y = document.querySelector(`.${clickedTag.text}`).getBoundingClientRect().top + window.pageYOffset - 65;
+            const y = document.querySelector(`.${clickedTag.text}`).getBoundingClientRect().top + window.pageYOffset - 89;
             window.scrollTo({ top: y, behavior: 'smooth' });
-            removeSelectedTags('nav_active');
+            removeSelectedTags('.nav', 'nav_active');
             selectCleckedTag(clickedTag, 'nav_active');
         }
     });
 }
 
-const removeSelectedTags = (active_class) => {
-    let tags = document.querySelectorAll('.navigation .nav');
+const removeSelectedTags = (searchClass, active_class) => {
+    let tags = document.querySelectorAll(searchClass);
     tags.forEach(tag => {
         tag.classList.remove(active_class);
     });
@@ -81,6 +81,26 @@ const slideMainDisplay = () => {
 
 // ############################################ Portfolio ###########################################
 
-const selectPortfolioTags = () => {
+const shufflePortfolio = () => {
 
+    document.querySelector('.portfolio__tags').addEventListener('click', (e) => {
+
+        if (e.target.classList.contains('tag')) {
+            let clickedTag = e.target;
+            removeSelectedTags('.tag', 'tag_active');
+            selectCleckedTag(clickedTag, 'tag_active');
+            // shufflePortfolioImg();
+            let images = document.querySelectorAll('.portfolio__picture');
+            let firstIndex = images[0].src.lastIndexOf('_') + 1;
+            let step = images[0].src.slice(firstIndex, -4);
+            let form = document.querySelector('.portfolio__pictures');
+            images.forEach(img => {
+                img.remove();
+                step === images.length ? step = 1 : ++step;
+                document.createElement('img');
+                img.src = `file:///home/dos/app/rs_school/singolo/assets/img/portfolio_${step}.png`
+                form.appendChild(img);
+            });
+        }
+    });
 }
